98a1f0ba8c2bd2157d29acebb53e3a73c502d165
daniilstudenikin <d.studenikin@action.tech>
2025-04-13 15:02:47 +0300

    init blogicum

---
+++ blogicum/blog/__init__.py
@@ -0,0 +1,6 @@
+from .urls import urlpatterns, app_name
+from .views import posts
+
+print(urlpatterns)
+print(app_name)
+print(posts)
---
+++ blogicum/pages/__init__.py
@@ -0,0 +1,4 @@
+from .urls import urlpatterns, app_name
+
+print(urlpatterns)
+print(app_name)
