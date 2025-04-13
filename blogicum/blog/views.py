c30cc5a51081ad8f42db6fa551c5df2f959acf18
daniilstudenikin <d.studenikin@action.tech>
2025-04-13 16:48:12 +0300

    Refactor code for improved readability and formatting

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -6,8 +6,11 @@ from .models import Category, Post
 def index(request):
     current_time = timezone.now()
 
-    post_list = Post.objects.filter(is_published=True, category__is_published=True,
-                                    pub_date__lt=current_time).order_by('-created_at')[:5]
+    post_list = Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lt=current_time
+    ).order_by('-created_at')[:5]
 
     context = {
         'post_list': post_list,
@@ -17,8 +20,13 @@ def index(request):
 
 def post_detail(request, post_id):
     current_time = timezone.now()
-    post = get_object_or_404(Post, id=post_id, is_published=True,
-                             category__is_published=True, pub_date__lt=current_time)
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        is_published=True,
+        category__is_published=True,
+        pub_date__lt=current_time
+    )
 
     context = {
         'post': post
