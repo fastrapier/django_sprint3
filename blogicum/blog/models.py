af89aeeeebc665607dc868733d4c0b31ef83b002
daniilstudenikin <d.studenikin@action.tech>
2025-04-13 16:36:15 +0300

    Refactor templates and models: improve structure, add new post card component, and enhance field verbose names

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class Location(models.Model):
-    name = models.CharField(max_length=256, verbose_name='Название')
+    name = models.CharField(max_length=256, verbose_name='Название места')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано', help_text='Снимите галочку, чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
 
     class Meta:
         verbose_name = 'местоположение'
@@ -19,7 +19,7 @@ class Category(models.Model):
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(unique=True, verbose_name='Идентификатор', help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано', help_text='Снимите галочку, чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
 
     class Meta:
         verbose_name = 'категория'
@@ -37,7 +37,7 @@ class Post(models.Model):
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, verbose_name='Местоположение')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name='Категория')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано', help_text='Снимите галочку, чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
 
     class Meta:
         verbose_name = 'публикация'
